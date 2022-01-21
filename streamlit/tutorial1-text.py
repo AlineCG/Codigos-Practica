6d9cac8c37d49f45d4d1d93118f915212f30019d
AlineCG <77987644+AlineCG@users.noreply.github.com>
2022-01-21 16:59:06 -0300

    Delete tutorial1-text.py

--- streamlit/tutorial1-text.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-text1 = st.text_input('Enter message')
-st.write('The Entered message is', text1)
-#se ejecuta con streamlit run helloworld.py desde cmd en la carpeta donde se encuentre el .py
