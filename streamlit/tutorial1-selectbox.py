3b32bcc8e861a566f274a78dcb53b616adf27d40
AlineCG <77987644+AlineCG@users.noreply.github.com>
2022-01-21 17:00:13 -0300

    Delete tutorial1-selectbox.py

--- streamlit/tutorial1-selectbox.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-df = pd.read_csv("addresses.csv")
-
-option = st.selectbox(
-    'Which City do you like best?', df['ciudad'].unique())
-
-'You selected: ', option
